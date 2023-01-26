df6f58f13d6924d9048db0516190d9e9266816a4
Gabriel Luna <lunagabri@gmail.com>
2023-01-26 10:55:08 -0300

    Update App.js

--- App.js
+++ App.js
@@ -5,41 +5,94 @@ import {
   View,
   StatusBar,
   Button,
+  Platform,
 } from "react-native";
 
 import * as Notifications from "expo-notifications";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+/* Manipulador de eventos de notificação */
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldPlaySound: true,
+      shouldShowAlert: true,
+      shouldSetBadge: true,
+    };
+  },
+});
 
 export default function App() {
-  useEffect(() => {
-    /* Ouvinte de evento para as notificações recebidas */
-    Notifications.addNotificationReceivedListener();
+  const [dados, setDados] = useState(null);
 
-    /* Ouvinte de evento para as respostas dadas às notificações */
-    Notifications.addNotificationResponseReceivedListener();
+  useEffect(() => {
+    /* Necessário para iOS */
+    async function permissoesIos() {
+      return await Notifications.requestPermissionsAsync({
+        ios: {
+          allowAlert: true,
+          allowSound: true,
+          allowBadge: true,
+          allowAnnouncements: true,
+        },
+      });
+    }
+
+    permissoesIos();
+
+    /* Ouvinte de evento para as notificações recebidas, ou seja,
+    quando a notificação aparece no topo da tela do dispositivo. */
+    Notifications.addNotificationReceivedListener((notificacao) => {
+      console.log(notificacao);
+    });
+
+    /* Ouvinte de evento para as respostas dadas às notificações, ou seja,
+    quando o usuário interage (toca) na notificação. */
+    Notifications.addNotificationResponseReceivedListener((resposta) => {
+      console.log(resposta.notification.request.content.data);
+      setDados(resposta.notification.request.content.data);
+    });
   }, []);
+
+  const enviarMensagem = async () => {
+    const mensagem = {
+      title: "Lembrete! 😀",
+      body: "Não se esqueça de tomar água, senão você morre 💀!",
+      data: { usuario: "Tiago 👽", cidade: "São Paulo" },
+      sound: Platform.OS === "ios" ? "default" : "", // necessário pro iOS
+    };
+
+    /* Função de agendamento de notificações */
+    await Notifications.scheduleNotificationAsync({
+      content: mensagem,
+      trigger: { seconds: 5 },
+    });
+  };
 
   return (
     <>
       <StatusBar />
-      <SafeAreaView style={estilos.container}>
+      <SafeAreaView style={styles.container}>
         <Text>Exemplo de sistema de notificação local</Text>
-        <Button title="Disparar notificação" />
+        <Button title="Disparar notificação" onPress={enviarMensagem} />
+
+        {dados && (
+          <View style={styles.conteudo}>
+            <Text> {dados.usuario} </Text>
+            <Text> {dados.cidade} </Text>
+          </View>
+        )}
       </SafeAreaView>
     </>
   );
 }
 
-const estilos = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
-  },
-  conteudo: {
-    marginVertical: 8,
-    backgroundColor: "yellow",
   },
   conteudo: {
     marginVertical: 8,
